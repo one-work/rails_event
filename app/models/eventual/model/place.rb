8a46ed7c6ebb2d980e330a3b205530f394162af3
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-02 16:31:26 +0800

    forget

--- app/models/eventual/model/place.rb
+++ app/models/eventual/model/place.rb
@@ -5,14 +5,19 @@ module Eventual
     included do
       attribute :name, :string
       attribute :description, :string
+      attribute :address, :string
+      attribute :tel, :string
       attribute :color, :string
       attribute :seats_count, :integer, default: 0
       attribute :plans_count, :integer, default: 0
+      attribute :ref_ident, :string
 
-      belongs_to :organ, optional: true
-      belongs_to :area, optional: true
+      belongs_to :organ, class_name: 'Org::Organ', optional: true
+      belongs_to :area, class_name: 'Ship::Area', optional: true
+
       belongs_to :place_taxon, counter_cache: true, optional: true
       belongs_to :taxon, class_name: 'PlaceTaxon', optional: true, foreign_key: :place_taxon_id
+
       has_many :plans
       has_many :seats, dependent: :delete_all
 
