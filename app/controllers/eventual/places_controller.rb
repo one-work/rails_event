1b199f77db770e5b0b0842669ce04c58083e6ba3
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-05 09:47:46 +0800

    support filte by area

--- app/controllers/eventual/places_controller.rb
+++ app/controllers/eventual/places_controller.rb
@@ -1,11 +1,16 @@
 module Eventual
   class PlacesController < BaseController
     before_action :set_place, only: [:show]
+    before_action :set_areas, only: [:index]
+    before_action :set_area, only: [:index]
 
     def index
       q_params = {}
       q_params.merge! default_params
       q_params.merge! params.permit(:place_taxon_id)
+      if params[:area_id]
+        q_params.merge! place_id: Place.where(area_id: params[:area_id]).pluck(:id)
+      end
 
       @place_taxons = PlaceTaxon.default_where(default_params)
       @places = Place.default_where(q_params).page(params[:page])
@@ -18,6 +23,19 @@ module Eventual
 
     def place_params
       params.fetch(:place, {})
+    end
+
+    def set_areas
+      area = Area.find_by(full: '河北') || Area.first
+      @areas = area.children
+    end
+
+    def set_area
+      if params[:area_id]
+        @area = Area.find params[:area_id]
+      else
+        @area = Area.find_by(full: '河北') || Area.first
+      end
     end
 
   end
