cd41cbd7bbd8896602cae353fa0f2f5b36d8e3fc
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-08 22:46:26 +0800

    palce

--- app/controllers/eventual/place/plans_controller.rb
+++ app/controllers/eventual/place/plans_controller.rb
@@ -24,14 +24,6 @@ module Eventual
       @plans = @plans.includes(:event, :hall).where(q_params)
     end
 
-    def place
-      @place = Place.find params[:place_id]
-      @plans = @event.plans.where(place_id: params[:place_id], plan_on: Date.today, plan_at: Time.current..).page(params[:page])
-      @plan_ons = @plans.distinct(:plan_on).select(:plan_on)
-
-      @plans = @plans.includes(:hall).order(plan_at: :asc)
-    end
-
     private
     def set_place
       @place = Place.find params[:place_id]
