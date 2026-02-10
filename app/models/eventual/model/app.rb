520bcbb2bdd67615da5dc8d2953776ca35c98d59
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-10 16:54:40 +0800

    event logic

--- app/models/eventual/model/app.rb
+++ app/models/eventual/model/app.rb
@@ -8,8 +8,17 @@ module Eventual
       attribute :appid, :string, index: true
       attribute :secret, :string
       attribute :base_url, :string
+      attribute :default, :boolean
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
+
+      scope :default, -> { where(default: true) }
+
+      after_save :set_default, if: -> { default? && saved_change_to_default? }
+    end
+
+    def set_default
+      self.class.where.not(id: self.id).where(organ_id: organ_id).update_all(default: false)
     end
 
   end
