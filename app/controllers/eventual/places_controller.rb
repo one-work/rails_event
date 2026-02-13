38a54d25ddd878d74ceafcbdbcd05b4baa9de15d
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-13 23:47:10 +0800

    defalt

--- app/controllers/eventual/places_controller.rb
+++ app/controllers/eventual/places_controller.rb
@@ -36,7 +36,7 @@ module Eventual
       else
         ip = Ship::Ip.find_or_create_by(ip_address: request.remote_ip)
         area = ip.area
-        @area = area || Area.find_by(full: '河北省') || Area.first
+        @area = area || Area.find_by(full: '石家庄市') || Area.first
       end
     end
 
