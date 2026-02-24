d8f6a64f1e4772c7bfeb65e6018116fafa29bf2b
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-24 09:16:49 +0800

    fix detail

--- app/models/eventual/place_taxon.rb
+++ app/models/eventual/place_taxon.rb
@@ -2,5 +2,6 @@ module Eventual
   class PlaceTaxon < ApplicationRecord
     include Model::PlaceTaxon
     include Com::Ext::Taxon
+    include Detail::Ext::TaxonModel if defined? RailsDetail
   end
 end
