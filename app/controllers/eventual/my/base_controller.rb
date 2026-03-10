e24a0883b79dedc1930509f1ff1961c000286697
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-10 16:24:20 +0800

    contact

--- app/controllers/eventual/my/base_controller.rb
+++ app/controllers/eventual/my/base_controller.rb
@@ -1,4 +1,14 @@
 module Eventual
   class My::BaseController < MyController
+
+    def set_cart
+      @cart = Trade::Cart.get_cart(
+        params,
+        good_type: 'Eventual::PlanJoin',
+        user_id: current_user.id,
+        **default_form_params
+      )
+    end
+
   end
 end
