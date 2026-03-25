a145ae3a57b6477f5f3b46e083430f9c0801e854
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-25 11:56:40 +0800

    filter bar

--- app/controllers/eventual/plans_controller.rb
+++ app/controllers/eventual/plans_controller.rb
@@ -6,9 +6,9 @@ module Eventual
 
     def index
       q_params = {
-        plan_on: Date.today
+        'plan_on' => Date.today
       }
-      q_params.merge! params.permit(:plan_on)
+      q_params.merge! params.permit(:plan_on, 'place.name-like')
       if @area
         area_ids = @area.self_and_descendant_ids
         q_params.merge! place_id: Place.where(area_id: area_ids).pluck(:id)
@@ -17,7 +17,7 @@ module Eventual
 
       @plan_ons = @event.plans.where(plan_on: Date.today..).distinct(:plan_on).order(:plan_on).select(:plan_on).limit(4)
       place_ids = @event.plans.where(plan_at: 1.hour.since..).where(q_params).select(:place_id, :plan_on).distinct.map(&:place_id)
-      @place_plans = PlacePlan.where(place_id: place_ids, event_id: @event.id, **q_params.slice(:plan_on)).includes(:plans).page(params[:page])
+      @place_plans = PlacePlan.includes(:place, :plans).where(place_id: place_ids, event_id: @event.id).default_where(q_params.slice('plan_on', 'place.name-like')).page(params[:page])
     end
 
     def place
